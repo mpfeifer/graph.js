802d0c3e0e7c7aed18f18a1ccf6428275c13ebd6
Matthias Pfeifer <m.pfeifer@diskover.de>
2015-07-17 20:46:44 +0200

    Initial Commit

---
+++ graph.js
@@ -0,0 +1,23 @@
+/**
+ Software-architecture
+
+GraphCanvas
+ - LogBook
+   - open
+   - close
+   - export to text
+   
+ - Graph Design Functionality
+   - Line Mode
+   - Node/Edge Mode
+   - Reset
+   - Undo
+   - Import
+   - Export
+
+Graph (undirected, no multiple edges)
+ - Nodes (nodes have ids (not names))
+ - Edges (edges have ids)
+ - properties for nodes and edges and graphs
+
+*/
